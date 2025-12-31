d19b2c2cd5cde80eba156afaaa5fb1e13c12f773
darshan <darshandumaraliya@gmail.com>
2025-12-31 17:42:05 +0530

    Changes done.

--- src/server.ts
+++ src/server.ts
@@ -43,35 +43,35 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-    const allowedOrigins = [
-      'https://aklinks.in',
-      'https://ads.aklinks.in',
-      'http://localhost:4200',
-    ];
+// app.use((req, res, next) => {
+//     const allowedOrigins = [
+//       'https://aklinks.in',
+//       'https://ads.aklinks.in',
+//       'http://localhost:4200',
+//     ];
 
-    const origin = req.headers.origin as string;
+//     const origin = req.headers.origin as string;
 
-    if (allowedOrigins.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-    }
+//     if (allowedOrigins.includes(origin)) {
+//       res.header('Access-Control-Allow-Origin', origin);
+//     }
 
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, PATCH, DELETE, OPTIONS'
-    );
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//     );
+//     res.header(
+//       'Access-Control-Allow-Methods',
+//       'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+//     );
 
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
+//     if (req.method === 'OPTIONS') {
+//       return res.sendStatus(204);
+//     }
 
-    next();
-});
+//     next();
+// });
 // app.use(cors());
 
 /* ===========================
